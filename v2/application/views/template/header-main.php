4085a69f3e211519a1cdcfa4cd5b511c278fc2a2
honeynatividad <honeynatividad@gmail.com>
2017-03-28 19:15:35 +0800

    js

--- v2/application/views/template/header-main.php
+++ v2/application/views/template/header-main.php
@@ -33,6 +33,8 @@
 
 	<script type="text/javascript" src="<?php echo base_url('assets/js/core/app.js') ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/pages/user_pages_profile.js') ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/pages/table_responsive.js') ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/plugins/tables/footable/footable.min.js') ?>"></script>
 	<!-- /theme JS files -->
 
 </head>
