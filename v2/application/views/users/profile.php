ed2c410f4befb20c23d2dd81574d27e0169666df
honeynatividad <honeynatividad@gmail.com>
2017-04-01 17:08:06 +0800

    prof

---
+++ v2/application/views/users/profile.php
@@ -0,0 +1,44 @@
+	
+
+			<!-- Banner -->	
+			<div class="profile-cover no-break-20">
+				<div class="profile-cover-img" style="background-image: url(<?php echo base_url('assets/images/placeholder.jpg') ?>"></div>
+					<div class="media">
+						 <div class="media-body">
+							<h1>Profile</h1>
+						</div>
+					</div>
+			</div>
+			<!-- /banner -->
+
+
+			<!-- Breadrumbs -->
+			<div class="content-group-lg border-top-lg border-red">
+				<div class="page-header page-header-default">
+					<div class="page-header-content1">
+						<div class="page-title">
+							 <ul class="breadcrumb">
+								<li><a href="https://www.gamensync.com/"><i class="icon-home2 icon-red position-left"></i> Home</a></li>
+								<li class="active">Profile</li>
+							</ul>
+						<a class="heading-elements-toggle"><i class="icon-more"></i></a></div>
+
+						<div class="heading-elements">
+							<form class="heading-form" action="#">
+								<div class="form-group">
+									<div class="has-feedback">
+										<input type="search" class="form-control" placeholder="Search">
+										<div class="form-control-feedback">
+											<i class="icon-search4 text-size-small text-muted"></i>
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /breadcrumbs -->
+
+		 
+				
